a218fd390cdd1d8477e8ceebc28533ded4551c16
Woloda <dorozovecvoloda@gmail.com>
2020-12-13 19:16:25 +0200

    Lab_10_1_C++

---
+++ Lab_10.1(C++)/Lab_10.1(C++).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+
+using namespace std;
+
+bool perevirka(char* fname);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char fname[51];
+	cout << "Введіть назву фізичного файлу: ";
+	cin >> fname;
+	cout << endl;
+
+	bool b = perevirka(fname);
+
+	if (b)
+		cout << "Так є серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+	else
+		cout << "Ні не має серед цих символів пара сусідніх букв “SQ” або ”QS”." << endl;
+
+	return 0;
+}
+
+bool perevirka(char* fname)
+{
+	ifstream perev_f(fname);
+
+	string lit_r;
+
+	while (getline(perev_f, lit_r))
+	{
+		cout << "Літерний рядок зчитаний з фізичного файлу: ";
+		cout << lit_r << endl << endl;
+
+		for (int i = 0; i < lit_r.length(); i++)
+		{
+			if ((lit_r[i] == 'Q' && lit_r[i + 1] == 'S') || (lit_r[i] == 'S' && lit_r[i + 1] == 'Q'))
+				return true;
+		}
+	}
+	return false;
+}
---
+++ UnitTest(Lab_10.1(C++))/UnitTest(Lab_10.1(C++)).cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_10.1(C++)/Lab_10.1(C++).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab101C
+{
+	TEST_CLASS(UnitTestLab101C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[101] = "E:\\Моя папка. Навчання\\Student [IK-11] [Дорожовець]\\Lab_10.1(C++)\\Lab_10.1(C++)\\test.txt";
+
+			bool b = perevirka(fname);
+
+			Assert::AreEqual(b, true);
+		}
+	};
+}
